2363a03744e3d1925c4f124ba968a40e1610aed9
camiloP96 <cristiancami1@hotmail.com>
2026-07-26 18:50:41 -0500

    modificación ligera mov del enemigo

--- Assets/Scripts/EnemyMove.cs
+++ Assets/Scripts/EnemyMove.cs
@@ -29,36 +29,17 @@ public class EnemyMove : MonoBehaviour
         velocidadOriginal = navEnemigo.speed;
     }
 
-    /*
-     
-    void Update()
-{
-    if(enRango && !enemigoMuerto)
-    {
-        navEnemigo.SetDestination(jugador.transform.position);
-
-        float distancia = Vector3.Distance(transform.position, jugador.transform.position);
-        float velocidad = Mathf.Clamp(velocidadOriginal, 0f, velocidadEnemigo);
-
-        navEnemigo.speed = velocidad;
-
-        animator.SetFloat("Speed", navEnemigo.speed);
-    }
-}
-      
-     
-     */
-
     void Update()
     {
         if (enRango && !enemigoMuerto)
         {
-            Vector3 enemDireccion = transform.position + jugador.transform.position;
+            Vector3 enemDireccion = jugador.transform.position - transform.position;
             Vector3 enemDestino = transform.position + enemDireccion.normalized * acercarse;
             navEnemigo.SetDestination(enemDestino);
             float distancia = enemDireccion.magnitude;
-            float velocidad = Mathf.Clamp(velocidadOriginal * (acercarse / distancia), 0f, velocidadEnemigo);
-            navEnemigo.speed = velocidad;
+            float velocidad = Mathf.Clamp(distancia, 0f, velocidadEnemigo);
+            navEnemigo.speed = velocidad;   
+            
         }
     }
 
